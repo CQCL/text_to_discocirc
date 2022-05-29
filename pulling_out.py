0f489240172a42da1eea9af72126388a96f82ec0
ge32kes <benjamin@rodatz.de>
2022-05-29 17:47:04 +0200

    Fixing minor bugs and further testing

--- pulling_out.py
+++ pulling_out.py
@@ -6,6 +6,7 @@ def pull_out_of_frame(diagram, opening_box_index):
     opening_box = diagram.boxes[opening_box_index]
     closing_box_index = -1
     closing_name = "[\\" + diagram.boxes[opening_box_index].name[1:]
+    # TODO: current assumption that names are unique
     for i, box in enumerate(diagram.boxes[opening_box_index + 1:], opening_box_index + 1):
         if box.name == closing_name:
             closing_box_index = i
@@ -13,16 +14,15 @@ def pull_out_of_frame(diagram, opening_box_index):
             break
 
     # closing_box_index = [index for (index, box) in enumerate(diagram.boxes[opening_box_index + 1:]) if box.name == closing_name]
-
     # --------- 2. create sub_diagram and recurse ---------
-    # TODO: change dom and cod of sub_diagram to incorporate wires to the left and right of boxes
-    sub_diagram = Diagram(opening_box.cod,
-                          closing_box.dom,
+    sub_diagram = Diagram(diagram.layers[opening_box_index].cod,
+                          diagram.layers[closing_box_index].dom,
                           diagram.boxes[opening_box_index + 1:closing_box_index],
                           diagram.offsets[opening_box_index + 1:closing_box_index])
 
     sub_diagram = pulling_out_diagram(sub_diagram)
     # TODO: check is this assignment necessary? (just out of interest)
+    # sub_diagram.draw()
 
     # -------- 3. merge diagrams ---------
     merged_boxes = diagram.boxes[:opening_box_index + 1] \
@@ -33,7 +33,9 @@ def pull_out_of_frame(diagram, opening_box_index):
                         + sub_diagram.offsets \
                         + diagram.offsets[closing_box_index:]
 
-    merged = Diagram(diagram.dom, diagram.cod, merged_boxes, merged_offsets)
+    diagram = Diagram(diagram.dom, diagram.cod, merged_boxes, merged_offsets)
+
+
 
     # -------- 4. pull out for current frame ---------
 
@@ -41,32 +43,34 @@ def pull_out_of_frame(diagram, opening_box_index):
     for i, box in enumerate(diagram.boxes[opening_box_index + 1:closing_box_index], opening_box_index + 1):
         if box.dom == Ty():
             # swap till at the top of frame
-            for swap_index in range(i, opening_box_index, -1):
-                # TODO: implement swapping of boxes
-                print(i)
+            for swap_index in range(i, opening_box_index + 1, -1):
+                diagram = diagram.interchange(swap_index, swap_index - 1)
+                # diagram.draw()
+
+            noun_index = opening_box_index + 1
+            # diagram.draw()
 
             # pull outside of top of frame
-
             # TODO: we are currently hardcoding a -1 here as we assume that the box has more wires inside than outside
-            dom = opening_box.dom[:diagram.offsets[i] - 1] @ Ty('n') @ opening_box.dom[diagram.offsets[i] - 1:]
-            cod = opening_box.cod[:diagram.offsets[i]] @ Ty('n') @ opening_box.cod[diagram.offsets[i]:]
+            dom = opening_box.dom[:diagram.offsets[noun_index] - diagram.offsets[opening_box_index] - 1] @ box.cod @ opening_box.dom[diagram.offsets[noun_index] - diagram.offsets[opening_box_index] - 1:]
+            cod = opening_box.cod[:diagram.offsets[noun_index] - diagram.offsets[opening_box_index]] @ box.cod @ opening_box.cod[diagram.offsets[noun_index] - diagram.offsets[opening_box_index]:]
 
-            print(dom)
-
-            print("i", i, " opening_index", opening_box_index)
-            print(diagram.offsets[i])
             new_top = Box(opening_box.name, dom, cod)
 
-            print(diagram.boxes[opening_box_index])
             new_boxes = diagram.boxes[:opening_box_index] + [box, new_top] + diagram.boxes[opening_box_index + 2:]
-            new_offsets = diagram.offsets[:opening_box_index] + [1, 0] + diagram.offsets[opening_box_index + 2:]
+            new_offsets = diagram.offsets[:opening_box_index] + [diagram.offsets[noun_index] - 1, diagram.offsets[opening_box_index]] + diagram.offsets[opening_box_index + 2:]
 
             diagram = Diagram(diagram.dom, diagram.cod, new_boxes, new_offsets)
+
+            opening_box_index += 1
+            opening_box = new_top
+
+            # diagram.draw()
         # 4.2 for each state: pull it out
 
-    diagram.draw()
+    # diagram.draw()
 
-    return merged
+    return diagram
 
 
 def pulling_out_diagram(diagram):
