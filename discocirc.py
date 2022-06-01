6f207e3bd45574662a7e16b402f8ece87ba4349d
JosephNathaniel <93722876+JosephNathaniel@users.noreply.github.com>
2022-06-02 00:48:00 +0100

    fixed pulling out and s-type expansion

--- discocirc.py
+++ discocirc.py
@@ -11,40 +11,36 @@ from drag_up import drag_all
 from frame import Frame, decomp
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def expand_box(box):
+def expand_box(box, last_n_n):
     n, s = map(rigid.Ty, 'ns')
     if isinstance(box, Frame):
         name, dom, cod = box.name, box.dom, box.cod
         insides = [expand_diagram(b) for b in box._insides]
         box = Frame(name, dom, cod, insides, box._slots)
+    if s.objects[0] in box.dom.objects:
+        pos = box.dom.objects.index(s.objects[0])
+        left, right = box.dom[:pos], box.dom[pos+1:]
+        box = rigid.Box(box.name, left @ last_n_n @ right, box.cod)
     if s.objects[0] not in box.cod:
-        return box
+        return box, None
     assert box.cod.count(s) == 1
     n_n = n ** box.dom.count(n)
     pos = box.cod.objects.index(s.objects[0])
-    left, right = rigid.Id(box.cod[:pos]), rigid.Id(box.cod[pos+1:])
-    expander = rigid.Box('x', s, n_n)
-    return box >> left @ expander @ right
+    left, right = box.cod[:pos], box.cod[pos+1:]
+    if isinstance(box, Frame):
+        expanded = Frame(box.name, box.dom, left @ n_n @ right, box._insides, box._slots)
+    else:
+        expanded = rigid.Box(box.name, box.dom, left @ n_n @ right)
+    return expanded, n_n
 
 
 def expand_diagram(diagram):
     new_diag = rigid.Id(diagram.dom)
+    last_n_n = None
     for left, box, right in diagram.layers:
-        box = expand_box(box)
+        box, n_n = expand_box(box, last_n_n)
+        if n_n:
+            last_n_n = n_n
         left, right = map(rigid.Id, (left, right))
         try:
             new_diag = new_diag >> left @ box @ right
@@ -193,14 +189,13 @@ def convert_sentence(diagram):
             diags[i:i+2] = [(new_diag, div)]
         step = rigid.Id().tensor(*[d[0] for d in diags])
     step = pulling_out_diagram(step)
-    step = merge_x(decomp(expand_diagram(drag_all(step))))
-    # step = merge_x(expand_diagram(step))
-    # res = merge_x(decomp(step))
-    # res = drag_all(res)
-    # return res
+    step = drag_all(step)
+    step = expand_diagram(step)
+    step = decomp(step)
+
     return step
 
+def sentence2circ(parser, sentence):
+    return convert_sentence(parser.sentence2tree(sentence).to_biclosed_diagram())
 
-decomp = rigid.Functor(
-    ob=lambda x: x,
-    ar=lambda b: b._decompose() if hasattr(b, '_decompose') else b)
+
--- pulling_out.py
+++ pulling_out.py
@@ -1,10 +1,11 @@
-from discopy import Diagram, Ty, Box
+from discopy import Diagram, Ty, Box, rigid
 
-from discocirc import Frame, drag_all
-from generate_context_circuit import init_nouns
+from frame import Frame
+from drag_up import drag_all, drag_out
+from discocirc_utils import init_nouns
 
 
-def pull_out_of_frame(diagram, opening_box_index):
+def pull_out_of_frame_old(diagram, opening_box_index):
     # ---------- 1. find closing box -----------
     opening_box = diagram.boxes[opening_box_index]
     closing_box_index = -1
@@ -23,7 +24,7 @@ def pull_out_of_frame(diagram, opening_box_index):
                           diagram.boxes[opening_box_index + 1:closing_box_index],
                           diagram.offsets[opening_box_index + 1:closing_box_index])
 
-    sub_diagram = pulling_out_diagram(sub_diagram)
+    sub_diagram = pulling_out_diagram_old(sub_diagram)
     # TODO: check is this assignment necessary? (just out of interest)
     # sub_diagram.draw()
 
@@ -76,37 +77,59 @@ def pull_out_of_frame(diagram, opening_box_index):
     return diagram
 
 
-def pulling_out_diagram(diagram):
+def pulling_out_diagram_old(diagram):
     # find all frames
     # TODO: check: do we have to restart once we find a frame?
     for i, box in enumerate(diagram.boxes):
         # identify start of a frame
         if "[" in box.name and not '\\' in box.name:
-            diagram = pull_out_of_frame(diagram, i)
+            diagram = pull_out_of_frame_old(diagram, i)
 
     return diagram
 
-def pulling_out_diagram_new(diagram):
-    # find all frames
-    # TODO: check: do we have to restart once we find a frame?
-    for i, box in enumerate(diagram.boxes):
-        # identify start of a frame
+def pulling_out_diagram(diagram):
+    new_diag = rigid.Id(diagram.dom)
+    for left, box, right in diagram.layers:
         if not isinstance(box, Frame):
-            return   
-        
+            new_diag = new_diag >> rigid.Id(left) @ box @ rigid.Id(right)
+            continue
         insides_with_nouns_removed = []
         inside_nouns = []
+        num_pulled_out = 0
         for inside in box._insides:
-            x, y = pull_out_of_frame_new(inside)
+            x, y, num_nouns = pull_out_of_frame(inside)
+            num_pulled_out += num_nouns
             insides_with_nouns_removed.append(x)
             inside_nouns.append(y)
+        if num_pulled_out > 0:
+            pulled_out_nouns = rigid.Id()
+            for nouns in inside_nouns:
+                pulled_out_nouns = pulled_out_nouns @ nouns
+            new_diag = new_diag >> rigid.Id(left) @ pulled_out_nouns @ rigid.Id(right)
+        frame_domain = box.dom @ Ty('n') ** num_pulled_out
+        new_frame = Frame(box.name, frame_domain, box.cod, insides_with_nouns_removed, box._slots)
+        new_diag = new_diag >> rigid.Id(left) @ new_frame @ rigid.Id(right)
+    return new_diag
 
-    return diagram
-
-def pull_out_of_frame_new(diagram):
+def pull_out_of_frame(diagram):
+    diagram = pulling_out_diagram(diagram)
     diagram = drag_all(diagram)
+    num_nouns = init_nouns(diagram)+1
+    if num_nouns == 0:
+        return diagram, diagram[:0], 0
+    inside_nouns = diagram[:num_nouns]
+    nouns_offset_begin = inside_nouns.offsets[0]
+    nouns_offset_end = inside_nouns.offsets[-1]
+    pull_out_index = num_nouns
+    i = 0
+    for box, offset in zip(diagram[num_nouns:].boxes, diagram[num_nouns:].offsets):
+        if len(box.dom) <= num_nouns \
+            and offset >= nouns_offset_begin \
+            and offset + len(box.dom) - 1 <= nouns_offset_end:
+            diagram = drag_out(diagram, num_nouns+i, pull_out_index)
+            pull_out_index += 1
+        i += 1
 
-    no_nouns = init_nouns(diagram)+1
-    insides_with_nouns_removed = diagram[no_nouns:]
-    inside_nouns = diagram[:no_nouns]
-    return insides_with_nouns_removed, inside_nouns
+    new_insides = diagram[pull_out_index:]
+    pulled_out_stuff = diagram[:pull_out_index]
+    return new_insides, pulled_out_stuff, num_nouns
