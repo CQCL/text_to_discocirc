14e42e5a50983ece92e8c510cdbec60a82d19281
Razin Shaikh <razinshaikh@outlook.com>
2022-06-04 13:18:30 +0000

    Richie's fast nn functor

--- src/network/utils.py
+++ src/network/utils.py
@@ -13,6 +13,55 @@ def get_nn_functor(nn_boxes, wire_dim):
     f = Functor(ob=neural_ob, ar=neural_ar, ar_factory=Network)
     return f
 
+def make_lambda_layer(a, b):
+    return keras.layers.Lambda(lambda x: x[:, a:b])
+
+def get_fast_nn_functor(nn_boxes, wire_dim):
+    def neural_ob(t):
+        return PRO(len(t) * wire_dim)
+
+    def neural_ar(box):
+        return nn_boxes[box]
+    f = Functor(ob=neural_ob, ar=neural_ar, ar_factory=Network)
+
+    def fast_f(diagram):
+        # diagram.draw()
+        inputs = keras.Input(shape=(len(f(diagram.dom)),))
+        outputs = inputs
+        # print(f"{inputs=}")
+        for fol in diagram.foliation():
+            # fol.draw()
+            in_idx = 0
+            out_idx = 0
+            models = []
+            layers = fol.layers
+            inps = fol.dom
+            for i in range(len(fol)):
+                left, box, right = layers[i]
+                n_wires = len(f(inps[:in_idx+len(left)-out_idx]))
+                f_idx = len(f(inps[:in_idx]))
+                # f_left = len(f(left))
+                if f_idx < n_wires:
+                    # print('boo', i, f_idx, f_left, box)
+                    model = make_lambda_layer(f_idx,n_wires)(outputs)
+                    models.append(model)
+                f_dom = len(f(box.dom))
+                # print(f"hi {n_wires=} {f_dom=} {box=} {outputs=}")
+                model = make_lambda_layer(n_wires, n_wires+f_dom)(outputs)
+                models.append(f(box).model(model))
+                in_idx = len(left) - out_idx + len(box.dom)
+                out_idx = len(left @ box.cod)
+            if right:
+                model = make_lambda_layer(n_wires+f_dom, None)(outputs)
+                models.append(model)
+            outputs = keras.layers.Concatenate()(models)
+            # print(f"{outputs=}")
+        dom, cod = f(diagram.dom), f(diagram.cod)
+        model = keras.Model(inputs=inputs, outputs=outputs)
+        return Network(dom, cod, model)
+    return fast_f
+
+
 #TODO do not hard-code hidden layers
 def initialize_boxes(lexicon, wire_dimension, hidden_layers=[10, 10]):
     nn_boxes = {}
