29407b8d3eca25c995de83ffbebfbdf4c7277007
BenjaminRodatz <benjamin@rodatz.de>
2022-06-17 12:17:19 +0200

    Bug fixing, adding comments and removing unused code

--- src/discocirc/discocirc_utils.py
+++ src/discocirc/discocirc_utils.py
@@ -5,17 +5,15 @@ from discopy.monoidal import Functor
 
 def init_nouns(circ):
     """
-    takes in a circuit with some number of nouns as the initial boxes
-    returns the index of the last of these initial nouns
+    takes in a circuit with some number of states as the initial boxes
+    returns the index of the last of these initial states
     """
+    for i in range(len(circ.boxes) - 1):
+        if circ.boxes[i].dom == Ty() and circ.boxes[i + 1].dom != Ty():
+            return i
 
-    index = -1
-    for i in range(len(circ.boxes)-1):
-        if circ.boxes[i].dom ==Ty() and circ.boxes[i+1].dom != Ty():
-            index = i # index of the last n oun
-            break
+    return -1
 
-    return index
 
 def get_star_removal_functor():
     def star_removal_ob(ty):
--- src/discocirc/pulling_out.py
+++ src/discocirc/pulling_out.py
@@ -1,135 +1,91 @@
-from discopy import Diagram, Ty, Box, rigid
+from discopy import Ty, rigid
 
 from discocirc.frame import Frame
 from discocirc.drag_up import drag_all, drag_out
 from discocirc.discocirc_utils import init_nouns
 
 
-def pull_out_of_frame_old(diagram, opening_box_index):
-    # ---------- 1. find closing box -----------
-    opening_box = diagram.boxes[opening_box_index]
-    closing_box_index = -1
-    closing_name = "[\\" + diagram.boxes[opening_box_index].name[1:]
-    # TODO: current assumption that names are unique
-    for i, box in enumerate(diagram.boxes[opening_box_index + 1:], opening_box_index + 1):
-        if box.name == closing_name:
-            closing_box_index = i
-            closing_box = box
-            break
-
-    # closing_box_index = [index for (index, box) in enumerate(diagram.boxes[opening_box_index + 1:]) if box.name == closing_name]
-    # --------- 2. create sub_diagram and recurse ---------
-    sub_diagram = Diagram(diagram.layers[opening_box_index].cod,
-                          diagram.layers[closing_box_index].dom,
-                          diagram.boxes[opening_box_index + 1:closing_box_index],
-                          diagram.offsets[opening_box_index + 1:closing_box_index])
-
-    sub_diagram = pulling_out_diagram_old(sub_diagram)
-    # TODO: check is this assignment necessary? (just out of interest)
-    # sub_diagram.draw()
-
-    # -------- 3. merge diagrams ---------
-    merged_boxes = diagram.boxes[:opening_box_index + 1] \
-                   + sub_diagram.boxes \
-                   + diagram.boxes[closing_box_index:]
-
-    merged_offsets = diagram.offsets[:opening_box_index + 1] \
-                        + sub_diagram.offsets \
-                        + diagram.offsets[closing_box_index:]
-
-    diagram = Diagram(diagram.dom, diagram.cod, merged_boxes, merged_offsets)
-
-
-
-    # -------- 4. pull out for current frame ---------
-
-    # 4.1 find nouns in frame
-    for i, box in enumerate(diagram.boxes[opening_box_index + 1:closing_box_index], opening_box_index + 1):
-        if box.dom == Ty():
-            # swap till at the top of frame
-            for swap_index in range(i, opening_box_index + 1, -1):
-                diagram = diagram.interchange(swap_index, swap_index - 1)
-                # diagram.draw()
-
-            noun_index = opening_box_index + 1
-            # diagram.draw()
-
-            # pull outside of top of frame
-            # TODO: we are currently hardcoding a -1 here as we assume that the box has more wires inside than outside
-            dom = opening_box.dom[:diagram.offsets[noun_index] - diagram.offsets[opening_box_index] - 1] @ box.cod @ opening_box.dom[diagram.offsets[noun_index] - diagram.offsets[opening_box_index] - 1:]
-            cod = opening_box.cod[:diagram.offsets[noun_index] - diagram.offsets[opening_box_index]] @ box.cod @ opening_box.cod[diagram.offsets[noun_index] - diagram.offsets[opening_box_index]:]
-
-            new_top = Box(opening_box.name, dom, cod)
-
-            new_boxes = diagram.boxes[:opening_box_index] + [box, new_top] + diagram.boxes[opening_box_index + 2:]
-            new_offsets = diagram.offsets[:opening_box_index] + [diagram.offsets[noun_index] - 1, diagram.offsets[opening_box_index]] + diagram.offsets[opening_box_index + 2:]
-
-            diagram = Diagram(diagram.dom, diagram.cod, new_boxes, new_offsets)
-
-            opening_box_index += 1
-            opening_box = new_top
-
-            # diagram.draw()
-        # 4.2 for each state: pull it out
-
-    # diagram.draw()
-
-    return diagram
-
-
-def pulling_out_diagram_old(diagram):
-    # find all frames
-    # TODO: check: do we have to restart once we find a frame?
-    for i, box in enumerate(diagram.boxes):
-        # identify start of a frame
-        if "[" in box.name and not '\\' in box.name:
-            diagram = pull_out_of_frame_old(diagram, i)
-
-    return diagram
-
 def pulling_out_diagram(diagram):
+    """
+    Return diagram where all states inside of frames have been pulled out.
+
+    :param diagram: Diagram -- Original diagram.
+    :return: Diagram -- New diagram with states pulled out of frames.
+    """
     new_diag = rigid.Id(diagram.dom)
+
+    # Iterate over all layers and rebuild the diagram by pulling all insides out of frame
     for left, box, right in diagram.layers:
         if not isinstance(box, Frame):
+            # Current layer not a frame
             new_diag = new_diag >> rigid.Id(left) @ box @ rigid.Id(right)
             continue
-        insides_with_nouns_removed = []
-        inside_nouns = []
+
+        list_insides_without_nouns = []
+        list_former_inside_states = []
         num_pulled_out = 0
+
+        # Pull out each individual inside
         for inside in box._insides:
-            x, y, num_nouns = pull_out_of_frame(inside)
-            num_pulled_out += num_nouns
-            insides_with_nouns_removed.append(x)
-            inside_nouns.append(y)
+            new_insides, former_inside_states = pull_inside_out_of_frame(inside)
+            num_pulled_out += len(former_inside_states)
+            list_insides_without_nouns.append(new_insides)
+            list_former_inside_states.append(former_inside_states)
+
+        # Rebuild pulled out layer
         if num_pulled_out > 0:
-            pulled_out_nouns = rigid.Id()
-            for nouns in inside_nouns:
-                pulled_out_nouns = pulled_out_nouns @ nouns
-            new_diag = new_diag >> rigid.Id(left) @ pulled_out_nouns @ rigid.Id(right)
+            new_pulled_out_nouns = rigid.Id()
+            for states_diag in list_former_inside_states:
+                new_pulled_out_nouns = new_pulled_out_nouns @ states_diag
+            new_diag = new_diag >> rigid.Id(left) @ new_pulled_out_nouns @ rigid.Id(right)
+
         frame_domain = box.dom @ Ty('n') ** num_pulled_out
-        new_frame = Frame(box.name, frame_domain, box.cod, insides_with_nouns_removed, box._slots)
+        new_frame = Frame(box.name, frame_domain, box.cod, list_insides_without_nouns, box._slots)
         new_diag = new_diag >> rigid.Id(left) @ new_frame @ rigid.Id(right)
+
     return new_diag
 
-def pull_out_of_frame(diagram):
+
+def pull_inside_out_of_frame(diagram):
+    """
+    Return the updated insides of a frame after pulling out the states and boxes only acting on pulled out states.
+
+    :param diagram: (Single) insides of a frame to pull states out of.
+    :return: Diagram -- new insides,
+            Diag -- States and boxes acting on states which were pulled out.
+    """
+    # Iteratively pull out diagram
     diagram = pulling_out_diagram(diagram)
     diagram = drag_all(diagram)
-    num_nouns = init_nouns(diagram)+1
+
+    num_nouns = init_nouns(diagram) + 1
     if num_nouns == 0:
-        return diagram, diagram[:0], 0
+        # Nothing to pull out
+        return diagram, diagram[:0]
+
     inside_nouns = diagram[:num_nouns]
     nouns_offset_begin = inside_nouns.offsets[0]
     nouns_offset_end = inside_nouns.offsets[-1]
+
+    # Find all processes that only act on states that are being pulled out. These will be pulled out too
     pull_out_index = num_nouns
-    i = 0
+    addition_pull_outs = 0
     for box, offset in zip(diagram[num_nouns:].boxes, diagram[num_nouns:].offsets):
         if len(box.dom) <= num_nouns \
-            and offset >= nouns_offset_begin \
-            and offset + len(box.dom) - 1 <= nouns_offset_end:
-            diagram = drag_out(diagram, num_nouns+i, pull_out_index)
+                and offset >= nouns_offset_begin \
+                and offset + len(box.dom) - 1 <= nouns_offset_end:
+            diagram = drag_out(diagram, num_nouns + addition_pull_outs, pull_out_index)
             pull_out_index += 1
-        i += 1
+        addition_pull_outs += 1
 
+    # New insides are all remaining non-state boxes
     new_insides = diagram[pull_out_index:]
-    pulled_out_stuff = diagram[:pull_out_index]
-    return new_insides, pulled_out_stuff, num_nouns
+
+    # Build diagram of pulled out boxes without any identities surrounding the states
+    pulled_out_nouns = rigid.Id()
+    current_cod = Ty()
+    for state in diagram.boxes[:pull_out_index]:
+        pulled_out_nouns = pulled_out_nouns >> (rigid.Id(current_cod) @ state)
+        current_cod = current_cod @ state.cod
+
+    return new_insides, pulled_out_nouns
