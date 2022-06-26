fe4ab361ccd14da3b0c7117aeb3f8fd340ed8e3b
Benjamin Rodatz <benjamin@rodatz.de>
2022-06-27 01:05:37 +0200

    First attempts at pulling out based on CCG. More complex examples seem to work

--- src/discocirc/discocirc_new.py
+++ src/discocirc/discocirc_new.py
@@ -1,3 +1,4 @@
+# %%
 from __future__ import annotations
 from dataclasses import dataclass
 
@@ -9,10 +10,12 @@ from discopy.biclosed import Over, Under
 class Term:
     name: str
     ccg: biclosed.Ty
+    composite_ccg: biclosed.Ty
     args: list[Term]
 
     def __call__(self, x: Term) -> Term:
-        return Term(self.name, self.ccg, [*self.args, x])
+        return Term(self.name, self.ccg, self.composite_ccg, [*self.args, x])
+        # return Term(self.name, self.ccg, [*self.args, x])
 
     def __repr__(self) -> str:
         args = self.args
@@ -45,7 +48,7 @@ def make_term(diagram):
     terms = []
     for box, offset in zip(diagram.boxes, diagram.offsets):
         if not box.dom:  # is word
-            terms.append(Term(box.name, box.cod, []))
+            terms.append(Term(box.name, box.cod, box.cod, []))
         else:
             if len(box.dom) == 2:
                 if box.name.startswith("FA"):
@@ -58,7 +61,8 @@ def make_term(diagram):
                     term = Compose(terms[offset + 1], terms[offset])
                 else:
                     raise NotImplementedError
-                terms[offset:offset+2] = [term]
+                term.composite_ccg = box.cod
+                terms[offset:offset + 2] = [term]
             elif box.name == "Curry(BA(n >> s))":
                 terms[offset] = TR(terms[offset])
             else:
@@ -111,13 +115,148 @@ def decomp(term):
     if term is None:
         return None
     if isinstance(term, Compose):
-        dummy = Term('?', biclosed.Ty('n'), [])
+        dummy = Term('?', biclosed.Ty('n'), biclosed.Ty('n'), [])
         return term.func1(term.func2(dummy))
     args = [decomp(arg) for arg in term.args]
-    return Term(term.name, term.ccg, args)
+    return Term(term.name, term.ccg, term.ccg, args)
 
 
 def make_diagram(term):
     term = decomp(term)
     diags = map(make_diagram, term.args)
     return make_word(term.name, term.ccg, *diags)
+
+
+# %%
+from lambeq import BobcatParser
+
+parser = BobcatParser()
+
+
+# %%
+def is_hyperbox(ccg):
+    if isinstance(ccg, Over):
+        if isinstance(ccg.right, (Over, Under)):
+            return True
+    elif isinstance(ccg, Under):
+        if isinstance(ccg.left, (Over, Under)):
+            return True
+    return False
+
+
+# %%
+def normalpull(term):
+    ccg = term.args[0].ccg
+    new_left_ccg = term.ccg.left
+    new_right_ccg = term.ccg.right
+
+    internal_args = []
+    pulled_out_args = []
+    for ar in term.args[0].args:
+        if isinstance(ccg, Over):
+            # If the input is a hyperbox, don't pull it out
+            if is_hyperbox(ccg):
+                ccg = ccg.left
+                internal_args.append(ar)
+                continue
+
+            new_left_ccg = Over(new_left_ccg, ar.composite_ccg)
+            new_right_ccg = Over(new_right_ccg, ar.composite_ccg)
+            ccg = ccg.left
+            pulled_out_args.append(ar)
+
+        elif isinstance(ccg, Under):
+            # If the input should go into the hyperbox
+            if is_hyperbox(ccg):
+                ccg = ccg.right
+                internal_args.append(ar)
+                continue
+
+            new_left_ccg = Under(new_left_ccg, ar.composite_ccg)
+            new_right_ccg = Under(new_right_ccg, ar.composite_ccg)
+            ccg = ccg.right
+            pulled_out_args.append(ar)
+
+    print('original:', term.ccg)
+    print(term)
+    term.args = [term.args[0]] + pulled_out_args + term.args[1:]
+    term.args[0].args = internal_args
+    term.ccg.left = new_left_ccg
+    term.ccg.right = new_right_ccg
+    print('new', term.ccg)
+    print(term)
+    return term
+
+
+def hyperpull(term):
+    if isinstance(term.ccg, Over):
+        # If the hyperbox term has multiple holes (i.e after the first hole it's still a hyperbox), continue pulling
+        if is_hyperbox(term.ccg.left):
+            new_term = hyperpull(Term('', term.ccg.left, term.ccg.left, term.args[1:]))
+            term.ccg.left = new_term.ccg
+            term.args = [term.args[0]] + new_term.args
+
+        if len(term.args) > 0:
+            if term.ccg.right != term.args[0].ccg:
+                print('pull out')
+                term = normalpull(term)
+            else:
+                print('combining types')
+                # term.composite_ccg = term.ccg.left
+    elif isinstance(term.ccg, Under):
+        # print('left is input')
+        if is_hyperbox(term.ccg.right):
+            new_term = hyperpull(Term('', term.ccg.right, term.ccg.right, term.args[1:]))
+            term.ccg.right = new_term.ccg
+            term.args = [term.args[0]] + new_term.args
+
+        if len(term.args) > 0:
+            if term.ccg.left != term.args[0].ccg:
+                print('pull out')
+                term = normalpull(term)
+            else:
+                print('combining types')
+                # term.composite_ccg = term.ccg.right
+    return term
+
+
+# %%
+def recurse_pull(term):
+    for i in range(len(term.args)):
+        term.args[i] = recurse_pull(term.args[i])
+    print(term.name, term.ccg)
+    if is_hyperbox(term.ccg):
+        print('This is a hyperbox. Might need to pull out')
+        term = hyperpull(term)
+    return term
+
+
+# %%
+
+def run_sentence(sentence):
+    ccg_parse = parser.sentence2tree(sentence).to_biclosed_diagram()
+    term = make_term(ccg_parse)
+    diag = make_diagram(term)
+    diag.draw()
+
+    new_term = recurse_pull(term)
+    new_diag = make_diagram(new_term)
+    new_diag.draw()
+
+
+sentences = [
+    # 'Alice quickly rapdily gives flowers to Bob',
+    # 'Alice quickly loves very red Bob',
+    # 'Alice fully loves Bob',
+    # 'Alice quickly eats',
+    # 'Alice quickly eats fish',
+    # 'Alice quickly eats red fish',
+    'Alice quickly rapidly loudly loves very very red Bob',
+    'Alice quickly and rapidly loves Bob and very red Claire'
+]
+for sentence in sentences:
+    run_sentence(sentence)
+
+
+
+# %%
