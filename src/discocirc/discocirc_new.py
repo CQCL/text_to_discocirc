1e1c7e96f916d329cd74cd28fb160d422fd4003c
Benjamin Rodatz <benjamin@rodatz.de>
2022-06-28 01:00:39 +0200

    Code clean up and bug fixing

--- src/discocirc/discocirc_new.py
+++ src/discocirc/discocirc_new.py
@@ -10,11 +10,11 @@ from discopy.biclosed import Over, Under
 class Term:
     name: str
     ccg: biclosed.Ty
-    composite_ccg: biclosed.Ty
+    output_ccg: biclosed.Ty
     args: list[Term]
 
     def __call__(self, x: Term) -> Term:
-        return Term(self.name, self.ccg, self.composite_ccg, [*self.args, x])
+        return Term(self.name, self.ccg, self.output_ccg, [*self.args, x])
         # return Term(self.name, self.ccg, [*self.args, x])
 
     def __repr__(self) -> str:
@@ -61,7 +61,7 @@ def make_term(diagram):
                     term = Compose(terms[offset + 1], terms[offset])
                 else:
                     raise NotImplementedError
-                term.composite_ccg = box.cod
+                term.output_ccg = box.cod
                 terms[offset:offset + 2] = [term]
             elif box.name == "Curry(BA(n >> s))":
                 terms[offset] = TR(terms[offset])
@@ -133,90 +133,100 @@ from lambeq import BobcatParser
 parser = BobcatParser()
 
 
+def type_check_term(term):
+    if len(term.args) == 0:
+        return term.ccg
+
+    for arg in term.args:
+        if type_check_term(arg) is None:
+            return None
+
+    if get_ccg_input(term.ccg) == term.args[0].ccg:
+        return get_ccg_output(term.ccg)
+    else:
+        return None
+
+
 # %%
 def is_hyperbox(ccg):
     if isinstance(ccg, Over):
-        if isinstance(ccg.right, (Over, Under)):
-            return True
+        return isinstance(ccg.right, (Over, Under))
     elif isinstance(ccg, Under):
-        if isinstance(ccg.left, (Over, Under)):
-            return True
+        return isinstance(ccg.left, (Over, Under))
+
     return False
 
 
-# %%
+def get_ccg_input(ccg):
+    if isinstance(ccg, Under):
+        return ccg.left
+    elif isinstance(ccg, Over):
+        return ccg.right
+
+
+def get_ccg_output(ccg):
+    if isinstance(ccg, Under):
+        return ccg.right
+    elif isinstance(ccg, Over):
+        return ccg.left
+
+
+def set_ccg_output(ccg, output):
+    if isinstance(ccg, Under):
+        ccg.right = output
+    elif isinstance(ccg, Over):
+        ccg.left = output
+
+
 def normalpull(term):
+    assert (is_hyperbox(term.ccg))
+
     ccg = term.args[0].ccg
-    new_left_ccg = term.ccg.left
-    new_right_ccg = term.ccg.right
-
-    internal_args = []
     pulled_out_args = []
-    for ar in term.args[0].args:
-        if isinstance(ccg, Over):
-            # If the input is a hyperbox, don't pull it out
-            if is_hyperbox(ccg):
-                ccg = ccg.left
-                internal_args.append(ar)
-                continue
+    for ar in term.args[0].args.copy():
+        # If current ccg is hyperbox, the input should go inside
+        # (by recursive property of pulling out, we assume all internal
+        # hyperboxes to already be pulled out correctly)
+        if is_hyperbox(ccg):
+            ccg = get_ccg_output(ccg)
+            continue
 
-            new_left_ccg = Over(new_left_ccg, ar.composite_ccg)
-            new_right_ccg = Over(new_right_ccg, ar.composite_ccg)
-            ccg = ccg.left
-            pulled_out_args.append(ar)
+        pulled_out_args.append((type(ccg), ar.output_ccg))
+        term.args.insert(1, ar)
+        term.args[0].args.remove(ar)
 
-        elif isinstance(ccg, Under):
-            # If the input should go into the hyperbox
-            if is_hyperbox(ccg):
-                ccg = ccg.right
-                internal_args.append(ar)
-                continue
+        ccg = get_ccg_output(ccg)
 
-            new_left_ccg = Under(new_left_ccg, ar.composite_ccg)
-            new_right_ccg = Under(new_right_ccg, ar.composite_ccg)
-            ccg = ccg.right
-            pulled_out_args.append(ar)
+    for ccg_type, ccg in reversed(pulled_out_args):
+        if ccg_type == Over:
+            term.ccg.left = Over(term.ccg.left, ccg)
+            term.ccg.right = Over(term.ccg.right, ccg)
+        elif ccg_type == Under:
+            term.ccg.left = Under(ccg, term.ccg.left)
+            term.ccg.right = Under(ccg, term.ccg.right)
 
-    print('original:', term.ccg)
-    print(term)
-    term.args = [term.args[0]] + pulled_out_args + term.args[1:]
-    term.args[0].args = internal_args
-    term.ccg.left = new_left_ccg
-    term.ccg.right = new_right_ccg
-    print('new', term.ccg)
-    print(term)
     return term
 
 
 def hyperpull(term):
-    if isinstance(term.ccg, Over):
-        # If the hyperbox term has multiple holes (i.e after the first hole it's still a hyperbox), continue pulling
-        if is_hyperbox(term.ccg.left):
-            new_term = hyperpull(Term('', term.ccg.left, term.ccg.left, term.args[1:]))
-            term.ccg.left = new_term.ccg
-            term.args = [term.args[0]] + new_term.args
+    assert (is_hyperbox(term.ccg))
 
-        if len(term.args) > 0:
-            if term.ccg.right != term.args[0].ccg:
-                print('pull out')
-                term = normalpull(term)
-            else:
-                print('combining types')
-                # term.composite_ccg = term.ccg.left
-    elif isinstance(term.ccg, Under):
-        # print('left is input')
-        if is_hyperbox(term.ccg.right):
-            new_term = hyperpull(Term('', term.ccg.right, term.ccg.right, term.args[1:]))
-            term.ccg.right = new_term.ccg
-            term.args = [term.args[0]] + new_term.args
+    if len(term.args) == 0:
+        # If no args, then nothing to pull out
+        return term
 
-        if len(term.args) > 0:
-            if term.ccg.left != term.args[0].ccg:
-                print('pull out')
-                term = normalpull(term)
-            else:
-                print('combining types')
-                # term.composite_ccg = term.ccg.right
+    # If the hyperbox term has multiple holes
+    # (i.e after the first hole it's still a hyperbox), continue pulling
+    if is_hyperbox(get_ccg_output(term.ccg)):
+        new_term = hyperpull(
+            Term('', get_ccg_output(term.ccg),
+                 get_ccg_output(term.ccg), term.args[1:]))
+        set_ccg_output(term.ccg, new_term.ccg)
+        term.args = [term.args[0]] + new_term.args
+
+    if get_ccg_input(term.ccg) != term.args[0].ccg:
+        term = normalpull(term)
+
     return term
 
 
@@ -224,10 +234,10 @@ def hyperpull(term):
 def recurse_pull(term):
     for i in range(len(term.args)):
         term.args[i] = recurse_pull(term.args[i])
-    print(term.name, term.ccg)
+
     if is_hyperbox(term.ccg):
-        print('This is a hyperbox. Might need to pull out')
         term = hyperpull(term)
+
     return term
 
 
@@ -240,23 +250,26 @@ def run_sentence(sentence):
     diag.draw()
 
     new_term = recurse_pull(term)
+    print(type_check_term(new_term))
     new_diag = make_diagram(new_term)
+
     new_diag.draw()
 
 
 sentences = [
-    # 'Alice quickly rapdily gives flowers to Bob',
+    'Alice quickly rapidly gives flowers to Bob',
+    'Alice quickly gives flowers to Bob',
     # 'Alice quickly loves very red Bob',
     # 'Alice fully loves Bob',
     # 'Alice quickly eats',
     # 'Alice quickly eats fish',
     # 'Alice quickly eats red fish',
-    'Alice quickly rapidly loudly loves very very red Bob',
-    'Alice quickly and rapidly loves Bob and very red Claire'
+    # 'Alice quickly loves very red Bob',
+    # 'Alice quickly rapidly loudly loves very very red Bob',
+    # 'Alice quickly and rapidly loves Bob and very red Claire',
+    # 'Alice loves Bob',
 ]
 for sentence in sentences:
     run_sentence(sentence)
-
-
 
 # %%
