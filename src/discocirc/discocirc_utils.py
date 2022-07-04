817a37ac2c41cbe08b4feb16ae22972bf3f2d059
Benjamin Rodatz <benjamin@rodatz.de>
2022-07-04 16:16:23 +0200

    Restructuring code

--- src/discocirc/discocirc_utils.py
+++ src/discocirc/discocirc_utils.py
@@ -1,3 +1,4 @@
+from discopy.biclosed import Under, Over
 from discopy.rigid import Ty
 from discopy.rigid import Ty, Box
 from discopy.monoidal import Functor
@@ -24,4 +25,35 @@ def get_star_removal_functor():
 
     f = Functor(ob=star_removal_ob, ar=star_removal_ar)
     return f
-    
+
+
+def type_check_term(term):
+    """
+    Given a term, check if all the arguments match the required ccg.
+
+    :param term: Term - The term which should be type checked.
+    :return: None - If term does not type check.
+        ccg - The output type of the term, if it type checks.
+    """
+    ccg = term.ccg
+    for arg in term.args:
+        if not get_ccg_input(ccg) == type_check_term(arg):
+            return None
+        ccg = get_ccg_output(ccg)
+
+    return ccg
+
+
+def get_ccg_input(ccg):
+    if isinstance(ccg, Under):
+        return ccg.left
+    elif isinstance(ccg, Over):
+        return ccg.right
+
+
+def get_ccg_output(ccg):
+    if isinstance(ccg, Under):
+        return ccg.right
+    elif isinstance(ccg, Over):
+        return ccg.left
+
