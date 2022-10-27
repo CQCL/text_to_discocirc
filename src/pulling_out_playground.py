34305849e1909e8998795604563bd33ea88f495f
y-richie-y <richie.yeung1@gmail.com>
2022-10-27 13:59:18 +0100

    Pull out

--- src/pulling_out_playground.py
+++ src/pulling_out_playground.py
@@ -16,42 +16,72 @@ sentence = "Alice quickly passionately gives Bob flowers"
 diagram = parser.sentence2tree(sentence).to_biclosed_diagram()
 term = make_term(diagram)
 
+# %%
+def just(term):
+    name = term.name
+    simple_type = term.simple_type
+    return Term(name, simple_type, simple_type, [])
 
-#%%
+
+def pop(term):
+    name = term.name
+    simple_type = term.simple_type
+    final_type = term.final_type
+    last_arg = term.args[-1]
+
+    new_final_type = last_arg.final_type >> final_type
+    return Term(name, simple_type, new_final_type, term.args[:-1]), term.args[-1]
+
+
+def thrush(f, g, h):
+    typs = []
+    simple_type = f.simple_type
+    for arg in f.args:
+        typs.append(simple_type.input)
+        simple_type = simple_type.output
+    h_type = h.final_type
+    simple_type = (h_type >> simple_type.input) >> (h_type >> simple_type.output)
+    final_type = simple_type
+    for typ in typs[::-1]:
+        simple_type = typ >> simple_type
+    return Term(f.name, simple_type, final_type, f.args), g, h
+
+
+# %%
+def check(f, g, h):
+    # don't pull out higher order diagrams
+    if isinstance(h.final_type, Func):
+        return False
+    # if nothing in g
+    if f.simple_type.input == g.simple_type:
+        return False
+    # only pull out from a higher order box:
+    if not is_higher_order(f.final_type):
+        return False
+    return True
+
+
 def pull_out(term):
     if len(term.args) == 0:
         return term
-    if len(term.args[0].args) == 0:
-        return term
-    f = term
-    g = term.args[0]
-    if is_higher_order(g.args[-1].final_type):
-        return term
-    if not is_higher_order(f.simple_type):
-        return term
-    if f.simple_type.input == g.simple_type:
-        return term
-    if is_higher_order(g.simple_type):
-        if f.simple_type.input == g.simple_type.output:
-            return term
-    g = pull_out(g)
-    f.args[0] = g
-    h = g.args[-1]
-    # if isinstance(h, Func):
-    #     return term
-    h_type = h.final_type
-    f_prime_type = (h_type >> f.simple_type.input) >> (h_type >> f.simple_type.output)
+    f = just(term)
+    for arg in term.args:
+        new_arg = pull_out(arg)
+        if len(new_arg.args) > 0:
+            g, h = pop(new_arg)
+            assert new_arg == g(h)
+            if check(f, g, h):
+                f, g, h = thrush(f, g, h)
+                assert pull_out(g) == g
+                assert pull_out(h) == h
+                f = pull_out(f(g))(h)
+            else:
+                f = f(new_arg)
+        else:
+            f = f(new_arg)
 
-    g_prime = Term(g.name, g.simple_type, h_type >> g.final_type, g.args[:-1])
-    f_prime = Term(f.name, f_prime_type, f_prime_type, [])
+    return f
 
-    new_term = f_prime(g_prime)
-    new_term = new_term(h)
-    for args in f.args[1:]:
-        new_term = new_term(args)
-    new_term = pull_out(new_term)
-    # draw_term(new_term)
-    return new_term
 
 new_term = pull_out(term)
 
