5f8e7664424b524c240c85de4755624fafdc517c
JosephNathaniel <jonathonliuau@gmail.com>
2023-06-09 18:10:51 +0100

    more string conversion

--- discocirc/expr/ccg_to_expr.py
+++ discocirc/expr/ccg_to_expr.py
@@ -16,7 +16,7 @@ def ccg_to_expr(ccg_parse):
     # Rules with 0 children
     if ccg_parse.rule == CCGRule.LEXICAL:
         word_index = ccg_parse.text + '_' + str(ccg_parse.original.variable.fillers[0].index)
-        closed_type = ccg_cat_to_closed(ccg_parse.original.cat, word_index)
+        closed_type = ccg_cat_to_closed(ccg_parse.original.cat, str(word_index))
         result = Expr.literal(ccg_parse.text, closed_type)
 
     # Rules with 1 child
@@ -93,7 +93,7 @@ def ccg_to_expr(ccg_parse):
 def composition(ccg_parse, f, g):
     x = create_random_variable(g.typ.input)
     result = Expr.lmbda(x, f(g(x)))
-    original_typ = ccg_cat_to_closed(ccg_parse.original.cat, random.randint(100, 999))
+    original_typ = ccg_cat_to_closed(ccg_parse.original.cat, str(random.randint(100, 999)))
     index_mapping = create_index_mapping_dict(original_typ, result.typ)
     result = map_expr_indices(result, index_mapping)
     return result
