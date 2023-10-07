6d21f770be3d5192e4f6bcd1ab071ef207f537b1
JosephNathaniel <jonathonliuau@gmail.com>
2023-10-07 16:57:28 +0100

    significant changes to coord_expand : 1. trigger condition expanded 2. a recursion removed 3. lambda case implemented

--- discocirc/expr/coordination_expand.py
+++ discocirc/expr/coordination_expand.py
@@ -1,4 +1,5 @@
 from discocirc.expr.expr import Expr
+from discocirc.expr.pull_out import is_higher_order
 from discocirc.helpers.closed import Ty
 from discocirc.helpers.discocirc_utils import apply_at_root, change_expr_typ, count_applications, create_random_variable
 
@@ -20,27 +21,36 @@ def coordination_expand(expr):
                 funs.append(nth_arg.fun)
                 heads.append(nth_arg.head)
                 nth_arg = nth_arg.arg
-            # TRIGGER: if nth_arg is multiheaded n-type, then do surgery on the tree
-            if nth_arg.typ == Ty('n') and nth_arg.head and len(nth_arg.head) > 1:
-                var = create_random_variable(nth_arg.typ)
-                var.head = nth_arg.head
-                body = var
-                for f, head in zip(reversed(funs), reversed(heads)):
-                    f = coordination_expand(f)
-                    body = f(body)
-                    body.head = head
-                composition = Expr.lmbda(var, body)
-                nth_arg = change_expr_typ(nth_arg, composition.typ >> nth_arg.typ)
-                # attach 'composition' at root of nth_arg
-                nth_arg = apply_at_root(nth_arg, composition)
-                expr = change_expr_typ(nth_arg, expr.typ)
-                expr.head = original_head
-                # assert expr.arg.typ == Ty('n')
-                # if expr.arg.head:
-                #     assert len(expr.arg.head) <= 1
-                break # something changed - break out of loop and recurse on new expr
+            # TRIGGER for doing the surgery on the tree: 
+            # first check if nth_arg is multiheaded n-type nonliteral
+            if nth_arg.typ == Ty('n') and nth_arg.head and len(nth_arg.head) > 1 and nth_arg.expr_type != 'literal':
+                # also check the overall function of nth_arg is NOT already higher order
+                nth_arg_deep_fun = nth_arg
+                for _ in range(count_applications(nth_arg)):
+                    nth_arg_deep_fun = nth_arg_deep_fun.fun
+                if not is_higher_order(nth_arg_deep_fun.typ): 
+                    var = create_random_variable(nth_arg.typ)
+                    var.head = nth_arg.head
+                    body = var
+                    # create the 'composition' of all funs
+                    for f, head in zip(reversed(funs), reversed(heads)):
+                        # f = coordination_expand(f) # think this became unnecessary ?
+                        body = f(body)
+                        body.head = head
+                    composition = Expr.lmbda(var, body)
+                    # attach 'composition' at root of nth_arg
+                    nth_arg = change_expr_typ(nth_arg, composition.typ >> nth_arg.typ)
+                    nth_arg = apply_at_root(nth_arg, composition)
+                    expr = change_expr_typ(nth_arg, expr.typ)
+                    expr.head = original_head
+                    assert expr.arg == coordination_expand(expr.arg)
+                    break # something changed - break out of loop and recurse on new expr
+        # recursion at the end
         fun = coordination_expand(expr.fun)
         arg = coordination_expand(expr.arg) # expect that this one is redundant if the for loop triggered
         expr = fun(arg)
         expr.head = original_head
-    return expr
+    elif expr.expr_type == "lambda":
+        body = coordination_expand(expr.body)
+        expr = Expr.lmbda(expr.var, body)
+    return expr # recursion for other types?
